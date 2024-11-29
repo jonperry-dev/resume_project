fae4c92786ceaf4a7a9641a09e74c24869567797
Jon Perry <jon@jonperry.dev>
2024-11-29 14:28:07 -0600

    added support for lint/fmt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Initialization v3 - App Coming Soon!
-        </p>
+        <p>Initialization v4 - App Coming Soon!</p>
       </header>
     </div>
   );
