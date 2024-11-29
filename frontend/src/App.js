35d609a3eb6b34ce032474e5fffaeb169c5d562a
Jon Perry <jon@jonperry.dev>
2024-11-29 14:11:49 -0600

    update husky

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Initialization - App Coming Soon!
+          Initialization v3 - App Coming Soon!
         </p>
       </header>
     </div>
