b913764091108742be7d94dded7d5be837e734fa
Jon Perry <jon@jonperry.dev>
2024-12-04 14:32:12 -0600

    allow any origin

--- backend/app/server.py
+++ backend/app/server.py
@@ -38,7 +38,7 @@ pipe = ml_model.get_model()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
